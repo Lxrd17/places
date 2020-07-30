287f26163aa4dc2e6fdf327faf1d2a099eca9826
Rxssette543 <rossetteismael@gmail.com>
2020-07-29 19:28:27 -0500

    1.1

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+AOS.init({
+    easing: 'ease-in-out'
+});
+
+
+var image = document.getElementsByClassName('thumbnail');
+new simpleParallax(image, {
+    delay: .9,
+    transition: 'cubic-bezier(0,0,0,0)'
+});
+
+$(function () {
+    var selectedClass = "";
+    $(".filter").click(function () {
+        selectedClass = $(this).attr("data-rel");
+        $("#gallery").fadeTo(100, 0.1);
+        $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+        setTimeout(function () {
+            $("." + selectedClass).fadeIn().addClass('animation');
+            $("#gallery").fadeTo(300, 1);
+        }, 300);
+    });
+});
